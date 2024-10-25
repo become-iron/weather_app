ecd49ba0e8072d94ef4d63e4d73120583bc46fb1
Nikita Burkov <8668962+become-iron@users.noreply.github.com>
2024-10-25 17:57:00 +0900

    Code cleanup

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,20 +19,6 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     fetchWeather();
-
-    //   determinePosition().then((position) async {
-    //     print('Position: $position');
-    //     print(appConfig.weatherService.appId);
-    //     final data = await WeatherService.getWeatherData(position);
-    //     print(data.list[0].main);
-    //     print(data.list[0].weather);
-    //
-    //     for (var item in data.list.sublist(0, 10)) {
-    //       print(item.dt_txt);
-    //       print(DateTime.parse(item.dt_txt));
-    //       print(DateFormat.yMMMd().format(DateTime.parse(item.dt_txt)));
-    //     }
-    //   });
   }
 
   ForecastResponse? weather;
@@ -72,69 +58,14 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
-
-    // return Scaffold(
-    //   // appBar: AppBar(
-    //   //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    //   //   title: const Text('Weather App'),
-    //   //   centerTitle: true,
-    //   // ),
-    //   body: Center(
-    //     child: Column(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         const Text(
-    //           'You have pushed the button this many times:',
-    //         ),
-    //         Text(
-    //           '$_counter',
-    //           style: Theme.of(context).textTheme.headlineMedium,
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: _incrementCounter,
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.add),
-    //   ),
-    // );
   }
 
   Future<void> fetchWeather() async {
     final position = await determinePosition();
-
-    // print('Position: $position');
-    // print(appConfig.weatherService.appId);
-    final data = await WeatherService.getWeatherData(position);
-    // print(data.list[0].main);
-    // print(data.list[0].weather);
-
-    // List<WeatherData> processedWeatherData = [];
-
-    // for (final item in data.list.sublist(0, 12)) {
-    //   final date = DateTime.fromMillisecondsSinceEpoch(
-    //     item.dt * 1000,
-    //     isUtc: true,
-    //   ).toLocal();
-    //   print(date);
-    //   // print(item.dt_txt);
-    //   // print(DateTime.fromMillisecondsSinceEpoch(item.dt * 1000, isUtc: true));
-    //   // print(DateFormat('yyyy-MM-dd HH:mm:ss')
-    //   //     .parse(item.dt_txt, true)
-    //   //     .toLocal());
-    //   // print(DateTime.parse(item.dt_txt));
-    //   // print(DateTime.parse(item.dt_txt).toLocal());
-    //   // print(DateFormat.Hm().format(DateTime.parse(item.dt_txt).toLocal()));
-    //   processedWeatherData.add(WeatherData(
-    //     date: DateFormat.Hm().format(date),
-    //     temperature: item.main.temp.round(),
-    //     icon: const Icon(Symbols.cloudy),
-    //   ));
-    // }
+    final weather_ = await WeatherService.getWeatherData(position);
 
     setState(() {
-      weather = data;
+      weather = weather_;
     });
   }
 }
